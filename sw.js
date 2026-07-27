ac01e317f899e13ef96c92a2ea7ef808a3679e36
Stock <stockerand@gmail.com>
2026-07-27 07:47:08 -0700

    Update index.html, sw.js, and manifest.json to wf-v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fingerblock-v11";
+const CACHE = "fingerblock-wf-v1";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
